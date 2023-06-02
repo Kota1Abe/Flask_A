34673587b950528c07e369db530e1b3af87f4dcb
SosukeMikami <mikami.sosuke.in@mynavi.jp>
2023-06-02 05:11:59 +0000

    [add]正規表現エラーメッセージ追加

--- mikami/calcsalary_app/salaly/views.py
+++ mikami/calcsalary_app/salaly/views.py
@@ -12,6 +12,25 @@ def input():
 def output():
     if request.method == "POST":
         salaly_str = request.form["salaly"]
-        if re.match('^[0123456789]+$', salaly_str):
-            return "gg"
-        return "gg"
+        if not re.match('^[-]{0,1}[0123456789]+$', salaly_str):
+            flash("数値を入力してください")
+            return redirect(url_for("input"))
+        
+        salaly = int(salaly_str)
+        
+        if 10000000000 < salaly:
+            flash("給与には最大、9,999,999,999円で入力可能です")
+            return redirect(url_for("input"))
+        elif salaly < 0 :
+            flash("給与にはマイナスの値は入力できません")
+            return redirect(url_for("input"))
+
+        if salaly < 1000000:
+            text = "給与：{2}円の場合、支給額:{0}円、税額:{1}円です。".format(int(salaly * 0.9), int(salaly * 0.1), salaly)
+            print(text, end="")
+        else:
+            text = "給与：{2}円の場合、支給額:{0}円、税額:{1}円です。".format(int((salaly - 1000000) * 0.8 + 900000), int((salaly - 1000000) * 0.2 + 100000), salaly)
+            print(text, end="")
+        return render_template("output.html", text=text)
+    
+    return redirect(url_for("input"))
