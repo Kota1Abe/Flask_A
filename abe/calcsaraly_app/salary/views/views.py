586735673e16c94c48adbca35267345a91b64b63
Kota1Abe <abe.kota.eo@mynavi.jp>
2023-06-02 02:39:40 +0000

    calc_salary

--- abe/calcsaraly_app/salary/views/views.py
+++ abe/calcsaraly_app/salary/views/views.py
@@ -1,33 +1,42 @@
 from flask import request,redirect,url_for,render_template,flash,session
 from salary import app
+from decimal import Decimal , ROUND_HALF_UP
+
+def calcsalary(salary):
+    if salary >= 1000000:
+        tax=(salary-1000000)*0.2+100000
+
+    else:
+        tax=salary*0.1
+
+    tax=Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+    pay_amount=salary-tax
+    return pay_amount,tax
+        
+
+
+
+
 
 
 @app.route('/')
 def show_entries():
-    return "mikami"
+    return render_template("input.html")
+    #return "mikami"
     """
     if not session.get("logged_in"):
         return redirect(url_for('login'))
     return render_template("entries/index.html")
     """
-"""
+
 @app.route("/login",methods=["GET","POST"])
 def login():
     if request.method=="POST":
-        if request.form["username"]!=app.config["USERNAME"]:
-            flash("ユーザー名が異なります")
-        elif request.form["password"]!=app.config["PASSWORD"]:
-            flash("パスワードが異なります")
-        else:
-            session["logged_in"]=True
-            flash("ログインしました")
-            return redirect(url_for('show_entries'))
-    return render_template("login.html")
-    
-@app.route("/logout")
-def logout():
-    session.pop("logged_in",None)
-    flash("ログアウトしました")
-    return redirect(url_for('show_entries'))
+        salary=int(request.form["salary"])
+        pay_amount,tax=calcsalary(salary)
 
-"""
+    return render_template("output.html",salary=salary,pay_amount=pay_amount,tax=tax)
+    
+@app.route("/output",methods=["GET","POST"])
+def logout():
+    return render_template("input.html")
