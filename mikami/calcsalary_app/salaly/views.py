14341376a3ac9b0458e524d1b28ccae62b58e397
SosukeMikami <mikami.sosuke.in@mynavi.jp>
2023-06-02 00:51:53 +0000

    [add]input作成 値の正規表現チェックまで

--- mikami/calcsalary_app/salaly/views.py
+++ mikami/calcsalary_app/salaly/views.py
@@ -1,6 +1,17 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from salaly import app
 
+# 正規表現用のライブラリ
+import re
+
 @app.route("/")
-def show_entries():
+def input():
     return render_template("input.html")
+
+@app.route("/output", methods=["GET", "POST"])
+def output():
+    if request.method == "POST":
+        salaly_str = request.form["salaly"]
+        if re.match('^[0123456789]+$', salaly_str):
+            return "gg"
+        return "gg"
