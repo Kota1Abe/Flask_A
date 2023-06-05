ad4556fd0bd05d6da0f30e0e3149712b231cc4be
ParkWonjin-mj <park.wonjin.mz@mynavi.jp>
2023-06-05 05:44:09 +0000

    flask_0605

--- jin/0601/application/flask_blog/views/entries.py
+++ jin/0601/application/flask_blog/views/entries.py
@@ -1,35 +1,66 @@
-from flask import request,redirect,url_for,render_template,flash,session
-from flask_blog import app 
-from flask_blog import db 
+from flask import request, redirect, url_for, render_template, flash, session
+from flask_blog import app
+from flask_blog import db
 from flask_blog.models.entries import Entry
-@app.route("/")
-def show_entries():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
-    
-    entries = Entry.query.order_by(Entry.id.desc()).all()
-  
-    return render_template("entries/index.html",entries=entries)
+from flask_blog.views.views import login_required
 
-@app.route("/entries",methods=["POST"])
+@app.route('/')
+@login_required
+def show_entries():
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
+
+
+@app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
-  
     entry = Entry(
-        tittle=request.form["title"],
-        text=request.form["text"]
-    )
+            title=request.form['title'],
+            text=request.form['text']
+            )
     db.session.add(entry)
     db.session.commit()
-    flash("新しく記事が作成されました")
-    return redirect(url_for("show_entries"))
+    flash('新しく記事が作成されました')
+    return redirect(url_for('show_entries'))
 
-@app.route("/entries/new",methods=["GET"])
+
+@app.route('/entries/new', methods=['GET'])
+@login_required
 def new_entry():
-    if not session.get("logged_in"):
-        return redirect(url_for("login"))
-    return render_template("entries/new.html")
-  
+    return render_template('entries/new.html')
 
 
+@app.route('/entries/<int:id>', methods=['GET'])
+@login_required
+def show_entry(id):
+    entry = Entry.query.get(id)
+    return render_template('entries/show.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
+def edit_entry(id):
+    entry = Entry.query.get(id)
+    return render_template('entries/edit.html', entry=entry)
+
+
+@app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
+def update_entry(id):
+    entry = Entry.query.get(id)
+    entry.title = request.form['title']
+    entry.text = request.form['text']
+    db.session.merge(entry)
+    db.session.commit()
+    flash('記事が更新されました')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_entry(id):
+    entry = Entry.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash('投稿が削除されました')
+    return redirect(url_for('show_entries'))
---
+++ jin/holiday_app /holiday/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config.from_object('flask_blog.config') # APP . CONFIG <- FLASK_BLOG.CONFIG 
+
+db = SQLAlchemy(app)
+
+# import flask_blog.views
+
+from flask_blog.views import views,entries 
