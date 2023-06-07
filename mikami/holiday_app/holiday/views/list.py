51e5ba72173ecda4db246a7cb66e2dbf4a1c33d1
SosukeMikami <mikami.sosuke.in@mynavi.jp>
2023-06-07 01:27:38 +0000

    [add]一覧表を動的にする

--- mikami/holiday_app/holiday/views/list.py
+++ mikami/holiday_app/holiday/views/list.py
@@ -1,8 +1,10 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from holiday import app
-
 from holiday import db
+
+from holiday.models.mst_holiday import Holiday
 
 @app.route("/list")
 def show_list():
-    return render_template("list.html")
+    holiday_list = Holiday.query.order_by(Holiday.holi_date.desc()).all()
+    return render_template("list.html", holiday_list=holiday_list)
