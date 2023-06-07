e3de3ba7bcc2cbbbfcf27eb6b6b3b5655fedab06
SosukeMikami <mikami.sosuke.in@mynavi.jp>
2023-06-07 03:20:14 +0000

    [add]myapp 初期セットアップ

---
+++ mikami/myapp_app/myapp/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object("myapp.config")
+
+db = SQLAlchemy(app)
+
+from myapp.views import views
---
+++ mikami/myapp_app/myapp/views/views.py
@@ -0,0 +1,39 @@
+from flask import request, redirect, url_for, render_template, flash, session
+from myapp import app
+
+from functools import wraps
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+"""
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"]:
+            flash("ユーザー名が異なります", "danger")
+        elif request.form["password"] != app.config["PASSWORD"]:
+            flash("パスワードが異なります", "danger")
+        else:
+            flash("ログインしました", "info")
+            session["logged_in"] = True
+            return redirect(url_for("show_entries"))
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("ログアウトしました", "info")
+    return redirect(url_for("show_entries"))
+
+def login_required(view):
+    @wraps(view)
+    def inner(*args, **kwargs):
+        if not session.get("logged_in"):
+            return redirect(url_for("login"))
+        return view(*args, **kwargs)
+    return inner
+"""
