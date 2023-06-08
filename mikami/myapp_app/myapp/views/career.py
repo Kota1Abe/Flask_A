b0ff973884d1107c8c6e926d778d10b086374e1c
SosukeMikami <mikami.sosuke.in@mynavi.jp>
2023-06-08 06:38:19 +0000

    [add]スクレイピングから追加

--- mikami/myapp_app/myapp/views/career.py
+++ mikami/myapp_app/myapp/views/career.py
@@ -11,6 +11,9 @@ from myapp.views.views import login_required #, user_permission_check
 
 import re
 
+import requests
+from bs4 import BeautifulSoup
+
 @app.route("/user/<int:id>/career/add", methods=["GET", "POST"])
 @login_required
 def add_career(id):
@@ -71,7 +74,97 @@ def delete_career(userid, careerid):
 @login_required
 def add_w_career(id):
     if request.method == "POST":
-        return request.form["url"]
-        #return redirect(url_for("show_user", id=userid))
-    return render_template("career/update.html", id=id)
+        res = requests.get(request.form["url"])
+        soup = BeautifulSoup(res.text, "html.parser")
 
+        # 経歴
+        careerSelector = {
+            "text": ".fuhLwL span",
+            "position": ".eMhnQX h3",
+            "img": ".btVpRG img",
+            "date_s": "div time:nth-child(1)"
+        }
+
+        caree = []
+
+        elems =soup.select(".jxAaMM")
+
+        i = 0
+        for elem in elems:
+            careelist = {}
+            for key in careerSelector.keys():
+                if key == "img":
+                    print(key)
+                    if not bool(elem.select(careerSelector[key])):
+                        careelist[key] = None
+                        continue
+                    print(elem.select(careerSelector[key])[0].attrs["src"])
+                    careelist[key] = elem.select(careerSelector[key])[0].attrs["src"]
+                elif key == "url":
+                    print(key)
+                    if not bool(elem.select(careerSelector[key])):
+                        careelist[key] = None
+                        continue
+                    print(elem.select(careerSelector[key])[0].attrs["href"])
+                    careelist[key] = elem.select(careerSelector[key])[0].attrs["href"]
+                else:
+                    print(key)
+                    if not bool(elem.select(careerSelector[key])):
+                        careelist[key] = None
+                        continue
+                    print(elem.select(careerSelector[key])[0].string)
+                    careelist[key] = elem.select(careerSelector[key])[0].string
+            caree.append(careelist)
+            i += 1
+        
+        manth = {
+            "Jan": "01",
+            "Feb": "02",
+            "Mar": "03",
+            "Apr": "04",
+            "May": "05",
+            "Jun": "06",
+            "Jul": "07",
+            "Aug": "08",
+            "Sep": "09",
+            "Oct": "10",
+            "Nov": "11",
+            "Dec": "12",
+        }
+
+        for value in caree:
+            m = value["date_s"][0:3]
+            y = value["date_s"][4:8]
+            value["date_s"] = y + "-" + manth[m]
+
+
+        print(caree)
+        session["n"] = len(caree)
+        return render_template("career/update_w.html", id=id, caree=caree)
+        #return redirect(url_for("show_user", id=userid))
+    return render_template("career/add_w.html", id=id)
+
+@app.route("/user/<int:id>/career/add_wpost", methods=["POST"])
+@login_required
+def add_wpost_career(id):
+    for key in range(session["n"]):
+        key = str(key)
+        if not re.match('^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$', request.form["s_date["+key+"]"] + "-01"):
+            flash("日付が不正な値です。フォーマット例通りに入力してください(例：2000-01)", "danger")
+            return render_template("career/add_w.html", id=id)
+
+        if request.method == "POST":
+            career = Career(
+                user_id = id,
+                name = request.form["name["+key+"]"],
+                imgsrc = request.form["imgsrc["+key+"]"],
+                position = request.form["position["+key+"]"],
+                s_date = request.form["s_date["+key+"]"] + "-01"
+
+
+            )
+            db.session.add(career)
+            db.session.commit()
+    flash("経歴を追加しました")
+    return redirect(url_for("show_user", id=id))
+    #return render_template("career/input.html")
